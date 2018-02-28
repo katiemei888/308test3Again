d4c936fae2fbc6623c999c940f760db5966fea19
Katherine Mei <katiemei888@gmail.com>
2018-02-28 11:07:59 -0800

    Adding classes

---
+++ src/main/java/test3Try/test3Try/App.java
@@ -0,0 +1,13 @@
+package test3Try.test3Try;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
